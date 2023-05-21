d5f653a72dc57fd1f300883f289872bdd07e271c
Sean <133057088+seanmcauliffe-personal@users.noreply.github.com>
2023-05-21 19:40:30 -0400

    Update snowflakeConnector.py
    Added a module docstring at the start of the file.
    Renamed the module name to conform to snake_case naming style.
    Removed trailing whitespaces.
    Adjusted the import order so standard imports are placed before third-party ones.
    Changed the name of the connection variable in the global scope to conn to avoid redefining outer scope names.
    Renamed the connection argument in the execute_query and close_connection functions to conn for consistency.
    
    Signed-off-by: Sean <133057088+seanmcauliffe-personal@users.noreply.github.com>

--- snowflakeConnector.py
+++ snowflakeConnector.py
@@ -1,5 +1,9 @@
-import snowflake.connector
+"""
+Module for connecting and interacting with a Snowflake database.
+"""
+
 from typing import List, Dict, Any
+import snowflake.connector
 
 def create_connection(
     user: str, 
@@ -22,13 +26,13 @@ def create_connection(
     )
 
 def execute_query(
-    connection: snowflake.connector.SnowflakeConnection, 
+    conn: snowflake.connector.SnowflakeConnection, 
     query: str
 ) -> List[Dict[str, Any]]:
     """
     Execute a SQL query on a Snowflake database.
     """
-    with connection.cursor() as cursor:
+    with conn.cursor() as cursor:
         cursor.execute(query)
         # Get column names from the cursor description attribute
         column_names = [column[0] for column in cursor.description]
@@ -36,14 +40,15 @@ def execute_query(
         return [dict(zip(column_names, row)) for row in cursor.fetchall()]
 
 def close_connection(
-    connection: snowflake.connector.SnowflakeConnection
+    conn: snowflake.connector.SnowflakeConnection
 ) -> None:
     """
     Close the connection to a Snowflake database.
     """
-    connection.close()
+    conn.close()
 
 # Usage:
-connection = create_connection('USERNAME', 'PASSWORD', 'ACCOUNT_URL', 'WAREHOUSE', 'DATABASE', 'SCHEMA')
-results = execute_query(connection, 'SELECT * FROM table_name')
-close_connection(connection)
+conn = create_connection('USERNAME', 'PASSWORD', 'ACCOUNT_URL', 'WAREHOUSE', 'DATABASE', 'SCHEMA')
+results = execute_query(conn, 'SELECT * FROM table_name')
+close_connection(conn)
+
