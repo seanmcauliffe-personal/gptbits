394c7020046b2620e863d108c4de4460f1eaaabe
Sean <133057088+seanmcauliffe-personal@users.noreply.github.com>
2023-05-21 19:38:24 -0400

    Update wikipediaParser.py
    Added a module docstring at the start of the file.
    Renamed the module name to conform to snake_case naming style.
    Added a function docstring for parse_wikipedia_page.
    Renamed the variable p to paragraph to conform to snake_case naming style.
    Renamed the variables url, text, and links to wiki_url, parsed_text, and parsed_links, respectively, to avoid redefining outer scope names.
    Adjusted the import order so standard imports are placed before third-party ones.
    Changed the constant name "url" to "wiki_url" to conform to UPPER_CASE naming style.
    
    Signed-off-by: Sean <133057088+seanmcauliffe-personal@users.noreply.github.com>

--- wikipediaParser.py
+++ wikipediaParser.py
@@ -1,18 +1,32 @@
-from bs4 import BeautifulSoup
-import requests
-import re
-import textwrap
+"""
+Module for parsing Wikipedia pages.
+"""
 
-def parse_wikipedia_page(url):
-    response = requests.get(url)
+import re
+import requests
+import textwrap
+from bs4 import BeautifulSoup
+
+def parse_wikipedia_page(page_url):
+    """
+    Parse the given Wikipedia page and return the cleaned text and the links.
+
+    Args:
+        page_url (str): The URL of the Wikipedia page to parse.
+
+    Returns:
+        str: The cleaned text from the page.
+        list: The links from the page.
+    """
+    response = requests.get(page_url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     content = soup.find('div', {'id': 'mw-content-text'})
     paragraphs = content.find_all('p', recursive=False)
 
     cleaned_text = ""
-    for p in paragraphs:
-        text = p.get_text().strip()
+    for paragraph in paragraphs:
+        text = paragraph.get_text().strip()
         # Remove unnecessary whitespace, newlines, etc.
         text = re.sub(r'\s+', ' ', text)
         text = textwrap.fill(text, width=70)
@@ -20,12 +34,13 @@ def parse_wikipedia_page(url):
 
     # Extract hyperlinks
     links = [a['href'] for a in content.find_all('a', href=True)]
-    links = [url + link if link.startswith('/') else link for link in links]
+    links = [page_url + link if link.startswith('/') else link for link in links]
 
     return cleaned_text, links
 
-url = "https://en.wikipedia.org/wiki/OpenAI"
-text, links = parse_wikipedia_page(url)
+wiki_url = "https://en.wikipedia.org/wiki/OpenAI"
+parsed_text, parsed_links = parse_wikipedia_page(wiki_url)
 
-print("Cleaned Text:\n", text)
-print("\nLinks:\n", "\n".join(links))
+print("Cleaned Text:\n", parsed_text)
+print("\nLinks:\n", "\n".join(parsed_links))
+
